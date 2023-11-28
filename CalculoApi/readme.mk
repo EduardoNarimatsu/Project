38d96d17d0a85175b9e01ac07165415599b6dc65
Eduardo Narimatsu <eduardo.ekn@gmail.com>
2023-11-28 09:30:36 -0300

    alter readme

--- CalculoApi/readme.mk
+++ CalculoApi/readme.mk
@@ -5,4 +5,4 @@ Ao executar a API pelo Visual Studio, será exibido o swagger da aplicação.
 Onde deve ser inserido o valor inicial e a quantidade de meses.
 
 Não foi implemtada a interface em Angular, pois não tenho conhecimento a fundo na linguagem, mas será a próxima linguagem que irei estudar,
-assim que finalizar o curso que estou fazendo de Golang.
+assim que finalizar o curso que estou fazendo de Golang. 
